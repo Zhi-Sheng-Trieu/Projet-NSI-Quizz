34be0764ec6970660ff106ba8b66f4a9f1373491
zhisheng <zhaotrieu1+git@gmail.com>
2023-02-05 17:24:06 -0500

    ajout de fichier

---
+++ src/render/Renderer.py
@@ -0,0 +1,33 @@
+class Renderer:
+    # on fait des methodes pour chaque page et le header et le footer
+    @staticmethod
+    def get_header(title="Quizz"):
+        return '''
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+                <title>{}</title>
+                <link rel="stylesheet" type="text/css" href="css/main.css"/>
+                <link rel="icon" type="image/png" href="img/icon.png"/>
+                <script type="text/javascript" src="fonctionJS.js"></script>
+            </head>
+            <body>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="index.html">Accueil</a></li>
+                        </ul>
+                    </nav>
+                </header>
+        '''.format(title)
+
+    @staticmethod
+    def get_footer():
+        return '''
+            </body>
+        </html>
+        '''
+
+
+
