81d936bbfab48eac73ea6673834bf43c52d79bb6
zhisheng <zhishengtrieu@gmail.com>
2023-03-06 10:18:41 -0500

    prise en charge du score

--- app.py
+++ app.py
@@ -85,6 +85,7 @@ def quizz():
         for q in Q:
             html += """
             <div class="question">
+                <input type="hidden" name="Q{0}" value="{10}">
                 <h3>Question {0}</h3>
                 <p>{1}</p>
                 <div class="reponses">
@@ -99,7 +100,7 @@ def quizz():
                 </div>
             </div><br>
             """.format(i, q[1], "Q" + str(i) + "rep1", q[2], "Q" + str(i) + "rep2", q[3], "Q" + str(i) + "rep3", q[4],
-                       "Q" + str(i) + "rep4", q[5])
+                       "Q" + str(i) + "rep4", q[5], q[0])
             i += 1
         html += """
             <button type="submit" id="submit">Valider</button>
@@ -113,7 +114,62 @@ def quizz():
 
 @app.route('/score', methods=['POST'])
 def score():
-    return render_template('head.html', title='Score') + render_template('score.html')
+    if session['loggedin']:
+        if request.method == 'POST' :
+            if 'Rep1' in request.form and 'Rep2' in request.form and 'Rep3' in request.form and 'Rep4' in request.form :
+                score = 0
+                mesQ = open("questions.csv", "r", encoding="utf-8")  # On ouvre le fichier contenant les questions
+                mesQ.readline()
+                Q = {}
+                for lig in mesQ:  # On parcourt ligne par ligne le fichier
+                    lig = lig.rstrip("\n")  # On retire le retour a la ligne
+                    vals = lig.split(";")  # On retire les ";"
+                    Q[vals[0]] = vals
+                mesQ.close()  # On ferme le fichier
+                for i in range(1, 5):
+                    q = Q[request.form['Q' + str(i)]]
+                    print(request.form['Rep' + str(i)])
+                    if request.form['Rep' + str(i)] == q[6]:
+                        score += 3
+                    else:
+                        score -= 1
+                if score < 0:
+                    score = 0
+                session['score'] += score
+                cursor = mydb.cursor()
+                cursor.execute('UPDATE users SET score = %s WHERE username = %s', (session['score'], session['username']))
+                mydb.commit()
+                cursor.execute('SELECT * FROM users ORDER BY score DESC')
+                classement = cursor.fetchall()
+                html = """
+                <div id="quizz">
+                    <p>Vous avez fait un scrore de {}/12 ! Votre score total est de {}</p>
+                    <div class="classement">
+                        <p>Classement :</p>
+                        <ol>""".format(score, session['score'])
+                for i in range(len(classement)):
+                    html += """<li>{}</li>""".format(classement[i][0])
+                html += """</ol>
+                    </div><p>Correction :</p>"""
+                for i in range(1, 5):
+                    html += """
+                    <p>Question {} : {}</p>
+                    <p>Réponse juste : {}</p><br>""".format(i, Q[request.form['Q' + str(i)]][1],
+                                                            Q[request.form['Q' + str(i)]][
+                                                                int(Q[request.form['Q' + str(i)]][6]) + 1])
+                html += """
+                    <form method="POST" action="/quizz">
+                        <button>Nouvelle partie</button>
+                    </form>
+                    <br>
+                    <form method="POST" action="/add-question">
+                        <button>Proposer une nouvelle question</button>
+                    </form>
+                </div>"""
+                return render_template('head.html', title='Score') + html
+            else:
+                return render_template('head.html', title='Score') + "Vous n'avez pas répondu à toutes les questions !"
+    return render_template('head.html', title='Score') + render_template('home.html')
 
 
 @app.route('/add-question', methods=['GET', 'POST'])
